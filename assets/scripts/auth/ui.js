9b188c9a62dfcd0dbefaf4c848a2e4d2157f2a06
mjeder <matteder96@gmail.com>
2021-02-11 15:57:30 -0500

    Update scripts auth

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,50 @@
+const store = require('./../store')
+
+const signUpSucess = function (response) {
+  $('#alert-message').text('Thank you for signing up')
+  $('#sign-up').trigger('reset')
+}
+
+const signUpFailure = function (response) {
+  $('#alert-message').text('Something went wrong... please try again!')
+}
+
+const signInSuccess = function (response) {
+  store.user = response.user
+  $('#alert-message').text('Thank you for signing in')
+  $('#sign-in').trigger('reset')
+}
+
+const signInFailure = function (response) {
+  $('#alert-message').text('Something went wrong... please try again!')
+}
+
+const changePasswordSuccess = function (response) {
+  $('#alert-message').text('Password successfully changed!')
+  $('#change-password').trigger('reset')
+}
+
+const changePasswordFailure = function (response) {
+  $('#alert-message').text('Something went wrong... please try again!')
+}
+
+const signOutSuccess = function () {
+  $('#alert-message').text('Signed out successfully!')
+  $('form').trigger('reset')
+  store.user = null
+}
+
+const signOutFailure = function () {
+  $('#alert-message').text('Failed to sign out... please try again!')
+}
+
+module.exports = {
+  signUpSucess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}

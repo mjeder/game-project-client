46ab003a1448d888f3ac42d2e5847c5af7e5d83d
mjeder <matteder96@gmail.com>
2021-02-16 12:30:29 -0600

    Update alert message verbiage

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,45 +1,45 @@
 const store = require('./../store')
 
 const signUpSuccess = function (response) {
-  $('#sign-up-fail').text('SUCCESS!')
+  $('#sign-up-alert').text('SUCCESS!')
   // $('#sign-in-view').show()
   // $('#sign-up-view').hide()
 }
 
 const signUpFailure = function (response) {
-  $('#sign-up-fail').text('Hmm.. something went wrong. Please, try again.')
+  $('#sign-up-alert').text('Hmm.. something went wrong. Please, try again.')
 }
 
 const signInSuccess = function (response) {
   store.user = response.user
-  $('#sign-in-fail').text('SUCCESS!')
+  $('#sign-in-alert').text('SUCCESS!')
   // $('#welcome-user-view').show()
   // $('#sign-in-view').hide()
   $('#sign-in').trigger('reset')
 }
 
 const signInFailure = function (response) {
-  $('#sign-in-fail').text('Hmm.. something went wrong. Please, try again.')
+  $('#sign-in-alert').text('Hmm.. something went wrong. Please, try again.')
 }
 
 const changePasswordSuccess = function (response) {
-  $('#change-password-fail').text('SUCCESS!')
+  $('#change-password-alert').text('SUCCESS!')
   $('#change-password').trigger('reset')
 }
 
 const changePasswordFailure = function (response) {
-  $('#change-password-fail').text('Hmm.. something went wrong. Please, try again.')
+  $('#change-password-alert').text('Hmm.. something went wrong. Please, try again.')
 }
 
 const signOutSuccess = function () {
-  $('#sign-out-fail').text('SUCCESS!')
+  $('#sign-out-alert').text('SUCCESS!')
   // $('#welcome-view').show()
   $('form').trigger('reset')
   store.user = null
 }
 
 const signOutFailure = function () {
-  $('#sign-out-fail').text('Hmm.. something went wrong. Please, try again.')
+  $('#sign-out-alert').text('Hmm.. something went wrong. Please, try again.')
 }
 
 module.exports = {
